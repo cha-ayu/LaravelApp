61d6f002b8f0fb6ad99dd094c5291bf29579f47d
Ayumi Ito <pickles.the.frog.yu@gmail.com>
2020-01-15 17:40:01 +0900

    add tasks

--- app/Folder.php
+++ app/Folder.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+
+    //hasMany: テーブル間の関連性をモデルクラスで表現
+    //この場合、第二・第三引数は省略可
+    public function tasks()
+    {
+        return $this->hasMany('App\Task', 'folder_id', 'id');
+    }
 }
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Folder;
+use App\Folder;     //add
+use App\Task;       //add
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,12 +14,21 @@ class TaskController extends Controller
         //Folderモデルのallクラスメソッドで全てのフォルダデータをDBから取得
         $folders = Folder::all();
 
+        //選ばれたフォルダを取得
+        //フォルダテーブルからIDカラムが一致するデータを返す
+        $current_folder = Folder::find($id);
+
+        //選ばれたフォルダに紐づくタスクを取得
+        //whereメソッドでデータ取得し、getメソッドでSQLをDBに発行した結果を取得
+        $tasks = $current_folder->tasks()->get();
+
         //1.取得したデータをテンプレートに渡した結果を返している（テンプレファイル名, [渡すデータ]配列）
         //2.テンプレをレンダリングした結果のHTMLがブラウザにレスポンスされる
         //3.選択されたフォルダのidをテンプレに渡す
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }
